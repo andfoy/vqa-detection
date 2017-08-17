d16e20618deb7b41e06279c1dd707699d1663943
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-08-17 11:56:51 -0500

    Add corpus utils

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ Misc functions and class wrappers.
 import sys
 import time
 import torch
+import codecs
 from visdom import Visdom
 
 
@@ -36,3 +37,57 @@ class VisdomWrapper(Visdom):
 
     def plot_line(self, name, **kwargs):
         self.line(win=self.plots[name], **kwargs)
+
+
+class Dictionary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = []
+
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.idx2word.append(word)
+            self.word2idx[word] = len(self.idx2word) - 1
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.idx2word)
+
+
+class Corpus(object):
+    def __init__(self):
+        self.dictionary = Dictionary()
+
+    def add_to_corpus(self, line):
+        """Tokenizes a text line."""
+        # Add words to the dictionary
+        words = line.split() + ['<eos>']
+        # tokens = len(words)
+        for word in words:
+            self.dictionary.add_word(word)
+
+    def tokenize(self, line):
+        # Tokenize line contents
+        words = line.split() + ['<eos>']
+        tokens = len(words)
+        ids = torch.LongTensor(tokens)
+        token = 0
+        for word in words:
+            if word not in self.dictionary.word2idx:
+                word = '<unk>'
+            ids[token] = self.dictionary.word2idx[word]
+            token += 1
+
+        return ids
+
+    def tokenize_file(self, file_path):
+        tokens = []
+        with codecs.open(file_path, 'r', 'utf-8') as f:
+            for line in f:
+                words = line.split() + ['<eos>']
+                for word in words:
+                    if word not in self.dictionary.word2idx:
+                        word = '<unk>'
+                    token = self.dictionary.word2idx[word]
+                    tokens.append(token)
+        return torch.LongTensor(tokens)
